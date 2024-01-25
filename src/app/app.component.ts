b98a2185e71dac9c88fe2c0d3e072c0ce3358836
Guido Maiola <maiolaguido@gmail.com>
2024-01-25 00:53:06 -0300

    interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   nombre = 'Guido'
+  textoPlaceHolder = 'Escriba algo aquí'
+  disabled = true;
+  imgSrc = 'https://img.freepik.com/vector-gratis/ilustracion-icono-galeria_53876-27002.jpg'
 
   constructor() {
     setInterval( ()=> this.nombre = 'Guido Maiola' ,5000);
+    setInterval( () => this.disabled = false, 3000)
   }
 
   getSuma(n1: number, n2: number) {
